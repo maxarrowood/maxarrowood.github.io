85d0fece171b2e96d5c4cb194a14fa729e5754ae
Max A <arrowood.m1@stu.janesville.k12.wi.us>
2022-12-02 11:22:27 -0600

    update

--- morcalc/morcalc.js
+++ morcalc/morcalc.js
@@ -2,15 +2,22 @@ function calc() {
     var p = document.getElementById("p").value;
     p = Number(p);
     var r = document.getElementById("r").value;
-    r = Number(r);
+    r = Number(r) /100;
     var t = document.getElementById("t").value;
     t = Number(t);
     var m = document.getElementById("m").value;
     m = Number(m);
+    var n = 12;
 
-    b = p * (1 + (r/12))**(12*t) - m * ((Math.abs(1 + (r/12))**(12*t)) / (r / n))
+    b = p * (1 + (r/n)**(n*t)) - (m * ((1 + (r/n))**(n*t)-1)) / (r/n);
 
     var p = document.getElementById("out");
-    var out = ""
+    var outp = " ";
+    outp = outp + b;
+    p.innerHTML = outp;
 
+}
+
+function rst() {
+    document.getElementById("out").innerHTML = " ";
 }
